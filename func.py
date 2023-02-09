ef334111001c8cd4cb0b7ae97f3d5b6007dd537c
shashi_bhushan_ramachandra <shashi.bhushan.ramachandra@oracle.com>
2023-02-09 19:09:57 +0530

    fix

--- func.py
+++ func.py
@@ -187,7 +187,7 @@ def simple_message_loop(client, stream_id, initial_cursor):
             dbsqlurl = 'https://wwjfteltaqsqcy9-adsadw.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/_/sql'
             headers = {"Content-Type": "application/sql"}
             auth=(dbschema, dbpwd)
-            # r = requests.post(dbsqlurl, auth=auth, headers=headers, data=ins)
+            r = requests.post(dbsqlurl, auth=auth, headers=headers, data=ins)
             # historicaldata.to_csv('oci://'+bucket_name+'/historicaldata.csv',index=False,storage_options = {"config": configfile})
 
         # get_messages is a throttled method; clients should retrieve sufficiently large message
